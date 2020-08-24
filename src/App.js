a97c623982087d7b633282b327d2a12beb52b440
vall4251n <58278138+nvalline@users.noreply.github.com>
2020-08-24 15:35:27 -0600

    added tagline & basic styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Logo from './components/logo/Logo';
+import Tagline from './components/main/Tagline';
 
 import './App.css';
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <main className="container">
       <Logo />
+      <Tagline />
     </main>
   );
 }
---
+++ src/components/main/Tagline.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './tagline.css';
+
+function Tagline() {
+    return (
+        <div className="tagline">
+            <h1>Developing Your Projects For Modern Times</h1>
+        </div>
+    )
+}
+
+export default Tagline;
