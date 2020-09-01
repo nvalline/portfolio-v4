d6eb414f58637c3d80239da7da4b1506aa9498a2
vall4251n <58278138+nvalline@users.noreply.github.com>
2020-08-31 21:11:56 -0600

    adjusted styles

--- src/components/about/AboutContent.js
+++ src/components/about/AboutContent.js
@@ -12,6 +12,7 @@ function AboutContent() {
                 <p>I am a Full Stack Web Developer with a wide range of business experience in multiple industries.  Recently completed a certificate in Full Stack Development from the University of Utah where I developed skills in Javascript, React, CSS and other current programming languages.</p>
                 <p>Previous work with a start-up company refined many other related skills; like problem solving, delivering accurate work, time management & communication among all levels of the business.  Here I proudly leveraged my ability to see both the macro and micro levels of the business, I have implemented different policies, procedures and programs that have resulted in improved efficiencies.</p>
                 <p>I’m excited to incorporate these past experiences with my creativity and implement them into building quality experiences on the web.</p>
+                <hr />
                 <Social />
             </div>
             <div className="bio-image">
